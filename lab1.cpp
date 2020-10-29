c9b6a4caa2bcbd32cf7e43fb9c97e29924c7ea57
kmichalski1 <73621723+kmichalski1@users.noreply.github.com>
2020-10-29 13:40:00 +0100

    Update lab1.cpp

--- lab1.cpp
+++ lab1.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <cmath>
+
+struct Wektor2D
+{
+	int x;
+	int y;
+	void printCoordinates() { std::cout << "Wspolrzedne wektora: [ " << x << ", " << y << " ]" << '\n'; }
+	void printNorm() { std::cout << "Norma wektora: " << sqrt((float)x*x + y*y) << '\n'; }
+};
 
 int main()
 {
-    puts("Hello, World!");
+	Wektor2D Wektor;
+	Wektor.x = 3;
+	Wektor.y = 10;
+	Wektor.printCoordinates();
+	Wektor.printNorm();
 }
